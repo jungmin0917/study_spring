66113203fce0975749c257ab0781f8a3a798dab3
jungmin0917 <cloonds@gmail.com>
2023-04-11 21:32:06 +0900

    Spring2 until day16

--- board/src/main/java/com/example/board/controller/BoardController.java
+++ board/src/main/java/com/example/board/controller/BoardController.java
@@ -4,8 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.example.board.domain.vo.BoardVO;
 import com.example.board.service.BoardService;
 
 import lombok.extern.log4j.Log4j;
@@ -29,10 +32,29 @@ public class BoardController {
 	@Autowired
 	private BoardService boardService;
 	
+	// 전체 목록
 	@GetMapping("/list")
 	public void list(Model model) {
 		log.info("/list");
 		model.addAttribute("boardList", boardService.getList());
+	}
+	
+	// 등록 처리
+	@PostMapping("/register")
+	public String register(BoardVO boardVO, RedirectAttributes rttr) {
+		log.info("/register : " + boardVO); // boardVO 객체가 넘어왔는지도 확인
+		
+		boardService.register(boardVO);
+
+//		Session에 Flash 영역이 있고, redirect로 전송을 하면 request 영역이 초기화된다.
+//		초기화되기 전에 Flash 영역에 데이터를 저장해놓고, 초기화된 후 Flash 영역에서 데이터를 가지고 온다.
+//		데이터를 가져왔다면 Flash 영역에 있던 데이터는 사라진다.
+		
+//		model.addAttribute("bno", boardVO.getBno());
+		rttr.addFlashAttribute("bno", boardVO.getBno());
+		
+//		redirect로 이동할 때는, 경로 앞에 "redirect:"를 붙여 준다
+		return "redirect:/board/list"; // 등록 후 /board/list로 리다이렉트 (이전 Request 내용 비움)
 	}
 }
 
--- board/src/test/java/com/example/board/controller/BoardControllerTests.java
+++ board/src/test/java/com/example/board/controller/BoardControllerTests.java
@@ -1,9 +1,16 @@
 package com.example.board.controller;
 
+import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 import lombok.extern.log4j.Log4j;
 
@@ -17,5 +24,37 @@ import lombok.extern.log4j.Log4j;
 
 // 여기서 테스트하면 일일이 톰캣 재시작해서 화면 띄울 필요 없음.
 public class BoardControllerTests {
-
+	@Autowired
+	private WebApplicationContext webApplicationContext;
+	private MockMvc mockMvc; // 요청, 응답을 보내고 받을 수 있는 객체
+	
+	@Before // 다른 메소드보다 먼저 실행
+	public void setUp() {
+		this.mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+	}
+	
+//	@Test
+//	public void listTest() throws Exception {
+//		log.info(mockMvc.perform(MockMvcRequestBuilders.get("/board/list")).andReturn().getModelAndView().getModelMap());
+//	}
+	
+	@Test
+	public void registerTest() throws Exception{
+		log.info(mockMvc.perform(MockMvcRequestBuilders.post("/board/register")
+				.param("title", "테스트 새 글 제목")
+				.param("content", "테스트 새 글 내용")
+				.param("writer", "hgd0000")
+				).andReturn().getFlashMap());
+	}
+	
 }
+
+
+
+
+
+
+
+
+
+
