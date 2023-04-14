4a145ad53b85013319e1e3b6eff808cb8c4f68cc
Administrator <Administrator@A06>
2023-04-14 19:39:23 +0900

    Spring3 until day17

---
+++ board/src/main/java/com/example/board/controller/ReplyController.java
@@ -0,0 +1,46 @@
+package com.example.board.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.board.domain.vo.ReplyVO;
+import com.example.board.service.ReplyService;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j;
+
+@Log4j
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/replies/*")
+public class ReplyController {
+	private final ReplyService replyService;
+	
+	// ´ñ±Û µî·Ï
+
+	@PostMapping(value = "/new", consumes = "application/json", produces = {MediaType.TEXT_PLAIN_VALUE})
+	public ResponseEntity<String> create(@RequestBody ReplyVO replyVO) {
+		log.info("create ....... " + replyVO);
+		return replyService.register(replyVO) ? new ResponseEntity<>("success", HttpStatus.OK) : new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
