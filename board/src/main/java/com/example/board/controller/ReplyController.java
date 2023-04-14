89b47e8f09e2cc460be0955668bb001327947f6c
Administrator <Administrator@A06>
2023-04-14 21:54:40 +0900

    Spring3 until day18 halfway

--- board/src/main/java/com/example/board/controller/ReplyController.java
+++ board/src/main/java/com/example/board/controller/ReplyController.java
@@ -1,9 +1,13 @@
 package com.example.board.controller;
 
+import java.util.List;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,11 +27,18 @@ public class ReplyController {
 	private final ReplyService replyService;
 	
 	// 댓글 등록
-
 	@PostMapping(value = "/new", consumes = "application/json", produces = {MediaType.TEXT_PLAIN_VALUE})
 	public ResponseEntity<String> create(@RequestBody ReplyVO replyVO) {
 		log.info("create ....... " + replyVO);
-		return replyService.register(replyVO) ? new ResponseEntity<>("success", HttpStatus.OK) : new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+		return replyService.register(replyVO) ? new ResponseEntity<String>("success", HttpStatus.OK) : new ResponseEntity<String>(HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+	
+	// 게시글 댓글 전체 조회
+	@GetMapping(value = "/{bno}", produces = {MediaType.APPLICATION_XML_VALUE})
+	public ResponseEntity<List<ReplyVO>> getList(@PathVariable("bno") Long bno) { // URL경로의 bno값을 파라미터 bno와 매핑시켜줌
+		log.info("getList ...... " + bno);
+
+		return new ResponseEntity<List<ReplyVO>>(replyService.findAllByBNO(bno), HttpStatus.OK);
 	}
 }
 
