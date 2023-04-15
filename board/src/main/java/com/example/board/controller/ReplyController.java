bebc22c8c4a8a3da3d3408675695683649902f3a
jungmin0917 <cloonds@gmail.com>
2023-04-15 15:40:40 +0900

    Spring3 until day18

--- board/src/main/java/com/example/board/controller/ReplyController.java
+++ board/src/main/java/com/example/board/controller/ReplyController.java
@@ -5,12 +5,13 @@ import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.example.board.domain.vo.ReplyVO;
 import com.example.board.service.ReplyService;
@@ -20,7 +21,7 @@ import lombok.extern.log4j.Log4j;
 
 @Log4j
 
-@Controller
+@RestController
 @RequiredArgsConstructor
 @RequestMapping("/replies/*")
 public class ReplyController {
@@ -34,11 +35,26 @@ public class ReplyController {
 	}
 	
 	// 게시글 댓글 전체 조회
-	@GetMapping(value = "/{bno}", produces = {MediaType.APPLICATION_XML_VALUE})
-	public ResponseEntity<List<ReplyVO>> getList(@PathVariable("bno") Long bno) { // URL경로의 bno값을 파라미터 bno와 매핑시켜줌
+	@GetMapping(value = "/{bno}/{page}", produces = {MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_UTF8_VALUE})
+	public ResponseEntity<List<ReplyVO>> getList(@PathVariable("bno") Long bno, @PathVariable("page") int page) { // URL경로의 bno값을 파라미터 bno와 매핑시켜줌
 		log.info("getList ...... " + bno);
 
 		return new ResponseEntity<List<ReplyVO>>(replyService.findAllByBNO(bno), HttpStatus.OK);
+	}
+	
+	// 댓글 1개 조회
+	@GetMapping(value = "/{rno}", produces = {MediaType.APPLICATION_JSON_UTF8_VALUE})
+	public ResponseEntity<ReplyVO> getReply(@PathVariable("rno") Long rno){
+		log.info("getReply ....... " + rno);
+		
+		return new ResponseEntity<ReplyVO>(replyService.findByRNO(rno), HttpStatus.OK);
+	}
+	
+	// 댓글 삭제
+	@DeleteMapping(value = "/{rno}", produces = {MediaType.TEXT_PLAIN_VALUE})
+	public ResponseEntity<String> remove(@PathVariable("rno") Long rno) {
+		log.info("remove ....... " + rno);
+		return replyService.remove(rno) ? new ResponseEntity<String>("success", HttpStatus.OK) : new ResponseEntity<String>(HttpStatus.INTERNAL_SERVER_ERROR);
 	}
 }
 
